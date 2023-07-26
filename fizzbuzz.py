18f79dfc3ee4cd3893c61453cf15879001c04a30
jeff-dillon <dillonjg@gmail.com>
2023-07-26 13:48:15 -0400

    implement fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
 # add your code here
+for number in range(1,101):
+    by_five = number % 5 == 0
+    by_three = number % 3 == 0
 
+    if(by_five and by_three):
+        print("FizzBuzz")
+    elif(by_three):
+        print("Fizz")
+    elif(by_five):
+        print("Buzz")
+    else:
+        print(str(number))
